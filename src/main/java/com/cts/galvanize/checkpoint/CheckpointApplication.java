caee230b3eb19e4c213f0a87b9a5b41921559800
Mihir Parekh <mihir.m.parekh@gmail.com>
2021-05-13 19:38:13 -0400

    Camelize Test Completed

---
+++ src/main/java/com/cts/galvanize/checkpoint/CheckpointApplication.java
@@ -0,0 +1,13 @@
+package com.cts.galvanize.checkpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckpointApplication.class, args);
+	}
+
+}
